8b044091481f1fd61e7269822e0a0b2e906ab765
chummy-ch <chummy.tema@gmail.com>
2020-09-12 19:23:23 +0300

    code review

--- app/src/main/java/com/example/flickrwiththreads/UIThreadHelper.java
+++ app/src/main/java/com/example/flickrwiththreads/UIThreadHelper.java
@@ -1,5 +1,7 @@
 package com.example.flickrwiththreads;
 
+import android.app.Application;
+import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
